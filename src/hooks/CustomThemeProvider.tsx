22d8888dbd618c39410fdcefe8ac3c884e9aeabd
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-13 17:29:00 +0900

    Feat : "커스텀 테마 프로바이더 설정"

---
+++ src/hooks/CustomThemeProvider.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { themeState } from '../lib/atom/atom';
+import { theme } from '../styles/theme/theme';
+
+interface propsType {
+  children: React.ReactNode;
+}
+
+function CustomThemeProvider({ children }: propsType) {
+  const curTheme = useRecoilValue(themeState);
+  return (
+    <ThemeProvider
+      theme={curTheme === 'light' ? theme.lightTheme : theme.darkTheme}
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
+
+export default CustomThemeProvider;
