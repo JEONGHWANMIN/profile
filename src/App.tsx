654144099ea21a0f4d2161bef182a1eead1fc472
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-13 17:29:26 +0900

    Feat : "토글 컴포넌트 테스트"

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ThemeToggle from './components/atoms/ThemeToggle';
+
+function App() {
+  return (
+    <div className='App'>
+      I'M HwanJJang
+      <ThemeToggle />
+    </div>
+  );
+}
+
+export default App;
