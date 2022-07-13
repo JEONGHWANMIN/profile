d1771f793a914e43513868c4d1b79b55e416c71e
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-14 00:51:50 +0900

    Feat : "Header Test"

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import ThemeToggle from './components/atoms/ThemeToggle';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       I'M HwanJJang
       <ThemeToggle />
     </div>
