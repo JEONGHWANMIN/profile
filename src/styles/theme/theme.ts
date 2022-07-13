4ea38ae45cffb91096a28111ae268bd404af6de3
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-14 00:49:39 +0900

    Feat : "테마 타입 정의 추가"

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,10 +1,12 @@
-export const lightTheme = {
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
   bgColor: '#F8F7F4',
   textColor: '#31302E',
   borderColor: '1px solid #eaeaea',
 };
 
-export const darkTheme = {
+export const darkTheme: DefaultTheme = {
   bgColor: '#1E1E22',
   textColor: '#ccc',
   borderColor: '1px solid #2cd33',
@@ -14,3 +16,5 @@ export const theme = {
   lightTheme,
   darkTheme,
 };
+
+export default theme;
