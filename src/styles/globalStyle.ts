b993b84c4a3c7ba93fe84ffac3c93c59ba7c79cf
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-15 04:05:57 +0900

    Feat : "메인 컬러 추가"

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -9,6 +9,30 @@ interface themeType {
 
 const GlobalStyle = createGlobalStyle<{ theme: themeType }>`
   ${reset}
+  :root {
+    /* white */
+    --color-white-1:#fff; 
+    --color-white-2:#f7f7f7; 
+    --color-white-3:#eee;
+    --color-white-4:#e6e6e6; 
+    --color-white-5:#ddd; 
+    --color-white-6:#d5d5d5; 
+    --color-white-7:#ccc; 
+    --color-white-8:#c4c4c4; 
+    --color-white-9:#bbb; 
+    /* dark */
+    --color-dark-1:#080808; 
+    --color-dark-2:#111; 
+    --color-dark-3:#191919;
+    --color-dark-4:#222; 
+    --color-dark-5:#2a2a2a; 
+    --color-dark-6:#333; 
+    --color-dark-7:#3b3b3b; 
+    --color-dark-8:#444; 
+    --color-dark-9:#4c4c4c; 
+
+
+  }
   html {
     font-size: 62.5%;
   }
