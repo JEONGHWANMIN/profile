2ac4ee22547bd264653e82f628f7d47ebb630cc7
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-15 04:06:14 +0900

    Feat : "추가 ViewMore 컴포넌트"

---
+++ src/components/molecules/ViewMore.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { BsMouse } from 'react-icons/bs';
+import { FiChevronsDown } from 'react-icons/fi';
+import styled from 'styled-components';
+
+function ViewMore() {
+  return (
+    <ViewContainer>
+      <IconBox>
+        <BsMouse className='mouse' />
+        <FiChevronsDown className='down' />
+      </IconBox>
+    </ViewContainer>
+  );
+}
+
+export default ViewMore;
+
+const ViewContainer = styled.div`
+  width: 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const IconBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+  font-size: 3rem;
+
+  .mouse {
+    font-size: 4rem;
+  }
+  .down {
+    margin-top: 1rem;
+
+    animation: up-down 1s;
+    animation-iteration-count: infinite;
+
+    @keyframes up-down {
+      100% {
+        transform: translateY(0%);
+      }
+      50% {
+        transform: translateY(40%);
+      }
+      100% {
+        transform: translateY(0%);
+      }
+    }
+  }
+`;
