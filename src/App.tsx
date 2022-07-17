3a2ff29771f97b0efd21e4f88f804d02951da9b7
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-17 10:38:55 +0900

    Feat : "Scroll Snap Test"

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import ThemeToggle from './components/molecules/ThemeToggle';
 import Header from './components/Header';
 import ViewMore from './components/molecules/ViewMore';
+import Main from './components/templates/Main';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className='App'>
+    <Container className='App'>
       <Header />
-      <ThemeToggle />
       <ViewMore />
-    </div>
+      <ThemeToggle />
+      <Main />
+      <Main />
+      <Main />
+      <Main />
+      <Main />
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div``;
