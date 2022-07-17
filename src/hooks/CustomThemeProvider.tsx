ac1b78af936e65cb16ca895786afb8995192642a
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-18 01:55:30 +0900

    Comment : "추가 Comment"

--- src/hooks/CustomThemeProvider.tsx
+++ src/hooks/CustomThemeProvider.tsx
@@ -8,6 +8,7 @@ interface propsType {
   children: React.ReactNode;
 }
 
+// main.tsx에서 따로 훅 사용이 불가능 하기 때문에 Custom Provider를 만들어서 사용 가능하도록 설정
 function CustomThemeProvider({ children }: propsType) {
   const curTheme = useRecoilValue(themeState);
   return (
