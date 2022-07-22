059abbcf195f1dce82038dd832e8ab4b7849b89b
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-23 03:05:02 +0900

    Feat : "Color Change"

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,14 +1,14 @@
 import { DefaultTheme } from 'styled-components';
 
 export const lightTheme: DefaultTheme = {
-  bgColor: '#F8F7F4',
+  bgColor: '#f7f7f7',
   textColor: '#31302E',
   borderColor: '1px solid #eaeaea',
 };
 
 export const darkTheme: DefaultTheme = {
   bgColor: '#1E1E22',
-  textColor: '#ccc',
+  textColor: '#f7f7f7',
   borderColor: '1px solid #2cd33',
 };
 
