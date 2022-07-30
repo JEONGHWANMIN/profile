747530e0cc55b735c882a3ffb592699ad5a8c318
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-31 07:40:47 +0900

    Feat :"컴포넌트 변경  "

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import Header from './components/Header';
-import Main from './components/templates/Main';
+import Header from './components/Organism/Header';
 import Home from './components/templates/Home';
 import About from './components/templates/About';
 import Project from './components/templates/Project';
 import { useRecoilValue } from 'recoil';
 import { themeState } from './lib/atom/atom';
+import Footer from './components/Organism/Footer';
+import Award from './components/templates/Award';
 function App() {
-  const curTheme = useRecoilValue(themeState);
   return (
     <div>
       <Header />
       <Home />
-      <About curTheme={curTheme} />
+      <About />
       <Project />
-      <Main />
-      <Main />
+      <Award />
+      <Footer />
     </div>
   );
 }
