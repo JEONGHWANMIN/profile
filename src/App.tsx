1398a1f1560fbf76e04aaa7c0590414781b1fa8a
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-01 17:21:27 +0900

    Feat : "추가 리팩토링 , 테스트"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Header from './components/Organism/Header';
 import Home from './components/templates/Home';
 import About from './components/templates/About';
-import Project from './components/templates/Project';
-import { useRecoilValue } from 'recoil';
-import { themeState } from './lib/atom/atom';
+import Project from './components/templates/Projects';
 import Footer from './components/Organism/Footer';
-import Award from './components/templates/Award';
+import Spec from './components/templates/Spec';
+import Contact from './components/templates/Contact';
+
 function App() {
   return (
     <div>
@@ -13,7 +13,8 @@ function App() {
       <Home />
       <About />
       <Project />
-      <Award />
+      <Spec />
+      <Contact />
       <Footer />
     </div>
   );
